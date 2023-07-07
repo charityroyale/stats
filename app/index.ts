b70b0118424f1efd202bdab513ee7202a0ee005f
Patrick Obermueller <pat.obermueller@gmail.com>
2023-07-07 19:13:08 +0200

    feature: draw queryparam streamername to canvas

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,15 @@
-import express from "express";
+import express, { Request } from "express";
 import { createCanvas } from "canvas";
 
 const app = express();
 const port = 3000;
 
-app.get("/", async (req, res) => {
+app.get("/", async (req: Request, res) => {
   try {
     const width = 400;
     const height = 200;
+
+    const streamerName = req.query.streamername?.toString() ?? 'YourLovelyNameHere'
 
     const canvas = createCanvas(width, height);
     const ctx = canvas.getContext("2d");
@@ -16,7 +18,7 @@ app.get("/", async (req, res) => {
     ctx.fillRect(0, 0, width, height);
     ctx.fillStyle = "white";
     ctx.font = "48px serif";
-    ctx.fillText("Charity Royale!!", 50, 100);
+    ctx.fillText(streamerName, 10, 100);
 
     const buffer = canvas.toBuffer("image/png");
     res.set("Content-Type", "image/png");

e9ba84dd72597301c02f305b139c0ae3e34b541f
Patrick Obermüller <patrick.obermueller@willhaben.at>
2023-07-25 10:38:19 +0200

    feature: add missing path to const

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 export const FONT_PATH = `${__dirname}/font`
 
-export const IMG_PATH = ''
+export const IMG_PATH = `${__dirname}/img`
 export const IMG_DOWNLOADS_PATH = `${__dirname}/img/downloads`
